5079bb923b821b186efe5f3623d8127c715581dd
Billimarie Lubiano Robinson <work@billimarie.com>
2016-08-04 17:52:17 -0700

    Add GitHub ribbon

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,8 +2,11 @@
     <meta name="author" content="Billimarie Lubiano Robinson">
 
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
     <link rel='stylesheet' href='/css/app.css' type='text/css' />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.0/gh-fork-ribbon.min.css" />
+    <!--[if lt IE 9]>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.0/gh-fork-ribbon.ie.min.css" />
+    <![endif]-->
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="/js/all.js"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,14 @@
     @include( 'includes.head' )
 
   <body>
+
+    <a class="github-fork-ribbon" href="http://www.github.com/billimarie/spot-the-docs" title="Fork me on GitHub">Fork me on GitHub</a>
+
     <div class="wrap">
       <div class="container">
         <div class="content">
           <h1 class="title"><a href="/">Spot the Docs</a></h1>
-          
+
           @include ( 'includes.nav' )
 
           <h2 class="description">Browse the most recent GitHub documentation issues on open-source projects.</h2>
